6a5142e6a8b23c691eff023c0cd82468a4e5167b
Student <student@compass.local>
2025-12-16 12:20:35 +0600

    all logic to alert

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const button = document.getElementById('button')
 
 const customAlert = (message) => {
-    const overlay = document.createElement('div')
+    const overlay = document.createElement('div');
     overlay.className = 'overlay';
 
     document.body.append(overlay);
 
-}
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'content';
+    overlay.append(contentDiv);
 
+    const messageH1 = document.createElement('h1');
+    messageH1.textContent = message;
+    contentDiv.append(messageH1);
+
+    const closeButton = document.createElement('button');
+    closeButton.className = 'closeBtn';
+    closeButton.textContent = 'ok';
+    contentDiv.append(closeButton);
+
+    closeButton.addEventListener('click', () => {
+        overlay.remove();
+    })
+
+    overlay.addEventListener('click', (event) => {
+        if (event.target.className === 'overlay'){
+            overlay.remove();
+        }
+})
+}
 button.addEventListener('click', () => {
     customAlert('lern frontend')
+})
+
+button2.addEventListener('click', () => {
+    customAlert('Do homework')
+})
+
+button3.addEventListener('click', () => {
+    customAlert('Go to cinema')
 })
